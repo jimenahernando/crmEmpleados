1b46f8b8781563daefebd34f7e416bb25ca1ce26
jimena <jimenahernando@hotmail.com>
2021-08-21 02:20:19 +0200

    Componente appComponent. Maquetacion y funcionalidad

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Empleado } from './interfaces/empleado.interface';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'crmEmpleados';
+
+  arrEmpleados: Empleado[];
+
+  constructor(){
+    this.arrEmpleados = [
+      {
+        nombre: 'Graciela',
+        apellidos: 'Barrerira',
+        dni: '12987679',
+        email: 'lagachi@hotmail.com',
+        foto: 'https://randomuser.me/api/portraits/women/2.jpg',
+        departamento: 'direccion',
+        estado: true,
+      },
+      {
+        nombre: 'Cecilia',
+        apellidos: 'Hernando',
+        dni: '30495896',
+        email: 'ceciliahernando@live.com',
+        foto: 'https://randomuser.me/api/portraits/women/38.jpg',
+        departamento: 'marketing',
+        estado: true,
+      }
+    ];
+  }
+
+  onEmpleadoEnviado(pEmpleado: Empleado){
+    this.arrEmpleados.push(pEmpleado);
+    console.log(this.arrEmpleados)
+  }
 }
